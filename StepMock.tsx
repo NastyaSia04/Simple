ac2b8a667a51d3202b100785c35fa861b40a5305
NastyaSia04 <167083909+NastyaSia04@users.noreply.github.com>
2025-11-23 19:23:56 +0200

    Create  StepMock.tsx

---
+++ StepMock.tsx
@@ -0,0 +1,19 @@
+import type { Step } from "../widgets/ProcessStepsBlocks/model/types";
+
+export const mockProcessSteps: Step[] = [
+  {
+    id: 1,
+    title: "Подключите канал",
+    description: "Авторизуйтесь и добавьте нашего бота админом, чтобы подтвердить владение.",
+  },
+  {
+    id: 2,
+    title: "Добавьте конкурентов",
+    description: "Соберите watchlist и включите умные алерты: всплески, удачные посты, тренды.",
+  },
+  {
+    id: 3,
+    title: "Получайте рекомендации",
+    description: "ИИ-агент предложит план публикаций, А/В заголовки и лучшее время выхода.",
+  },
+];
